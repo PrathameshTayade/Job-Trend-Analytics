c53722e34ada6fb82312386889e5c7ad8669b567
Prathamesh Tayade <tayadeprathameshlearning25@gmail.com>
2026-06-19 18:27:56 +0000

    Updated register_Dim_Table.sql

--- Databricks/SQL/register_Dim_Table.sql
+++ Databricks/SQL/register_Dim_Table.sql
@@ -3,6 +3,6 @@
 
 DROP TABLE IF EXISTS {$Catalog}.presented_master.tb_dim_Employer;
 
-CREATE TABLE ddh_prod.presented_master.tb_dim_Employer
+CREATE TABLE {$Catalog}.presented_master.tb_dim_Employer
 USING DELTA
 LOCATION 'abfss://{lake_root}@stajobtrendanalytics.dfs.core.windows.net/Presented/dim_Employer/Schema=1';
